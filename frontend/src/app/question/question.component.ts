e711b8c2d224f9bef95dcbee9792d677ebaede9e
Todor <todor.ilievski@isys.de>
2020-04-30 10:05:16 +0200

    Add previous and next button on quesiton

--- frontend/src/app/question/question.component.ts
+++ frontend/src/app/question/question.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {QuestionService} from "../_common/services/question/question.service";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {Question} from "../_common/entities/question";
 
 @Component({
@@ -11,9 +11,11 @@ import {Question} from "../_common/entities/question";
 export class QuestionComponent implements OnInit {
 
   question: Question = new Question();
+  questionId: string;
 
   constructor(private questionService: QuestionService,
-              private route: ActivatedRoute) { }
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
     this.getQuestion();
@@ -21,10 +23,21 @@ export class QuestionComponent implements OnInit {
 
   getQuestion() {
     this.route.params.subscribe(params => {
+      this.questionId = params.id;
       this.questionService.getById<Question>(params.id).subscribe(response => {
         this.question = response;
       })
     })
   }
 
+  next() {
+    //should fix, ist not 30 a fix number , so this will work till 30, and should work will end of the questions.
+    if(this.questionId == '30') return 30;
+    return parseInt(this.questionId) + 1;
+  }
+
+  previous() {
+    if(this.questionId == '1') return 1;
+    return parseInt(this.questionId) - 1;
+  }
 }
